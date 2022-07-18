b249c27f1fa03f6071723eefe661a42b98f668ee
Iberi Daniel <danieliberi99@gmail.com>
2022-07-18 19:39:03 +0100

    prices

--- src/components/Menuitem/MenuItem.jsx
+++ src/components/Menuitem/MenuItem.jsx
@@ -1,11 +1,39 @@
 import React from 'react';
 
-import './MenuItem.css';
+import styled from 'styled-components';
 
-const MenuItem = () => (
-  <div>
-    MenuItem
-  </div>
+const Container = styled.div``
+
+const Head = styled.div``
+
+const Name = styled.div``
+
+const NameText = styled.p`
+  color: #DCCA87;
+`
+
+const Price = styled.div``
+
+const PriceText = styled.p`
+  color: #fff;
+`
+
+const MenuItem = ({ title, price, tags }) => (
+  <Container>
+    <Head>
+      <Name>
+        <NameText className='p__comorant'>
+          {title}
+        </NameText>
+      </Name>
+
+      <Price>
+        <PriceText className='p__comorant'>
+          {price}
+        </PriceText>
+      </Price>
+    </Head>
+  </Container>
 );
 
 export default MenuItem;
--- src/container/Menu/SpecialMenu.jsx
+++ src/container/Menu/SpecialMenu.jsx
@@ -1,11 +1,93 @@
 import React from 'react';
 
-import './SpecialMenu.css';
+import { images, data } from '../../constants';
+
+import styled from 'styled-components';
+
+import { SubHeading, MenuItem } from '../../components';
+
+const Container = styled.div`
+  flex-direction: column;
+  background: var(--color-black);
+`
+
+const MenuTitle = styled.div`
+  margin-bottom: 2rem;
+  text-align: center;
+`
+
+const MenuHeader = styled.div``
+
+const Menu = styled.div`
+  width: 100%;
+  margin: 2rem 0;
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
+  flex-direction: row;
+`
+
+const Left = styled.div``
+
+const LeftText = styled.p``
+
+const LeftMenuItems = styled.div``
+
+const Middle = styled.div``
+
+const MiddleImg = styled.img``
+
+const Right = styled.div``
+
+const RightText = styled.p``
+
+const RightMenuItems = styled.div``
+
+const ButtonContainer = styled.div`
+  margin-top: 15px;
+`
+
+const Button = styled.button``
 
 const SpecialMenu = () => (
-  <div>
-    SpecialMenu
-  </div>
+  <Container className='flex__center section__padding' id='menu'> {/* app__specialMenu */}
+    <MenuTitle> {/* app__specialMenu-title */}
+      <SubHeading title='Menu that fits your palette' />
+    </MenuTitle>
+    <MenuHeader className='headtext__cormorant'>Today's Special</MenuHeader> {/* app__specialMenu-h1 */}
+
+    <Menu> {/* app__specialMenu-menu */}
+      <Left className='flex__center'>
+        <LeftText> {/* app__specialMenu-heading */}
+          Wine and Beer
+        </LeftText>
+        <LeftMenuItems> {/* app__specialMenu-items */}
+          {data.wines.map((wine, index) => (
+            <MenuItem key={wine.title + index} title={wine.title} price={wine.price} tags={wine.tags} />
+          ))}
+        </LeftMenuItems>
+      </Left>
+
+      <Middle> {/* app__specialMenu-img */}
+        <MiddleImg src={images.menu} />
+      </Middle>
+
+      <Right>
+        <RightText> {/* app__specialMenu-heading */}
+          Wine and Beer
+        </RightText>
+        <RightMenuItems> {/* app__specialMenu-items */}
+          {data.cocktails.map((cocktail, index) => (
+            <MenuItem key={cocktail.title + index} title={cocktail.title} price={cocktail.price} tags={cocktail.tags} />
+          ))}
+        </RightMenuItems>
+      </Right>
+    </Menu>
+
+    <ButtonContainer>
+      <Button className='custom__button'>View More</Button>
+    </ButtonContainer>
+  </Container>
 );
 
 export default SpecialMenu;

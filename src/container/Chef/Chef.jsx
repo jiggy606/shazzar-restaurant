f1c07f59d940bd7edaf1319d6fd4dd51c326485a
Iberi Daniel <danieliberi99@gmail.com>
2022-07-19 18:59:47 +0100

    video done

--- src/container/Chef/Chef.jsx
+++ src/container/Chef/Chef.jsx
@@ -1,11 +1,96 @@
 import React from 'react';
 
-import { images, data } from '../../constants';
+import images from '../../constants/images'
+import styled from 'styled-components';
+
+import { SubHeading } from '../../components';
+
+const MajContainer = styled.div``
+
+const MinContainer = styled.div``
+
+const Image = styled.img``
+
+const Info = styled.div``
+
+const HeadText = styled.h1``
+
+const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  margin-top: 5rem;
+`
+
+const Quote = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  align-items: flex-end;
+`
+
+const QuoteImg = styled.img`
+  height: 40px;
+  width: 47px;
+  margin: 0 1rem 1rem 0;
+`
+
+const QuoteText = styled.p``
+
+const Sign = styled.div`
+  width: 100%;
+  margin-top: 3rem;
+`
+
+const SignName = styled.p`
+  color: var(--color-golden);
+  font-family: var(--font-base);
+  font-weight: 400;
+  font-size: 32;
+  line-height: 41px;
+  letter-spacing: 0.04em;
+  text-transform: capitalize;
+`
+
+const SignInfo = styled.p``
+
+const SignImg = styled.img`
+  width: 250px;
+  margin-top: 3rem;
+
+  @media screen and (max-width: 2000px) {
+    width: 370px;
+  }
+
+  @media screen and (max-width: 450px) {
+    width: 80%;
+  }
+`
 
 const Chef = () => (
-  <div>
-    Chef
-  </div>
+  <MajContainer className='app__bg app__wrapper section__padding'>
+    <MinContainer className='app__wrapper_img app__wrapper_img-reverse'>
+      <Image src={images.chef} />
+    </MinContainer>
+
+    <Info className='app__wrapper_info'>
+      <SubHeading title="Chef's work" />
+      <HeadText className='headtext__cormorant'>What We Believe In</HeadText>
+
+      <Content>
+        <Quote>
+          <QuoteImg src={images.quote} />
+          <QuoteText className='p__opensans'>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</QuoteText>
+        </Quote>
+        <QuoteText className='p__opensans'>Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</QuoteText>
+      </Content>
+
+      <Sign>
+        <SignName>Daniel Iberi</SignName>
+        <SignInfo className='p__opensans'>Chef & Founder</SignInfo>
+        <SignImg src={images.sign} />
+      </Sign>
+    </Info>
+  </MajContainer>
 );
 
 export default Chef;
--- src/container/Intro/Intro.jsx
+++ src/container/Intro/Intro.jsx
@@ -1,11 +1,61 @@
 import React from 'react';
 
-import './Intro.css';
+import { useRef, useState } from 'react';
 
-const Intro = () => (
-  <div>
-    Intro
-  </div>
-);
+import styled from 'styled-components';
+
+import { BsFillPlayFill, BsPauseFill } from 'react-icons/bs';
+
+import { meal } from '../../constants';
+
+const Container = styled.div`
+  height: 100vh;
+  position: relative;
+`
+const Video = styled.video`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+`
+const Overlay = styled.div`
+  position: absolute;
+  inset: 0;
+  background: rgba(0, 0, 0, 0.25);
+`
+const OverlayCircle = styled.div`
+  width: 100px;
+  height: 100px;
+  border-radius: 50%;
+  border: 1px solid var(--color-golden);
+  cursor: pointer;
+`
+
+const Intro = () => {
+
+  const [playVideo, setPlayVideo] = useState(false);
+  const vidRef = useRef()
+
+  const handleVideo = () => {
+    setPlayVideo((prevPlayVideo) => !prevPlayVideo);
+
+    if (playVideo) {
+      vidRef.current.pause();
+    } else {
+      vidRef.current.play();
+    }
+  }
+
+  return (
+    <Container> {/* app__video */}
+      <Video src={meal} type='video/mp4' loop controls={false} muted ref={vidRef} />
+
+      <Overlay className='flex__center'> {/* app__video-overlay */}
+        <OverlayCircle className='flex__center' onClick={handleVideo}> {/* app__video-overlay-circle */}
+          {playVideo ? <BsPauseFill color='#fff' fontSize={30} /> : <BsFillPlayFill color='#fff' fontSize={30} />}
+        </OverlayCircle>
+      </Overlay>
+    </Container>
+  );
+};
 
 export default Intro;
